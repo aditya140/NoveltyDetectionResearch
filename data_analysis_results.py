d03946a1a1448560690030ca03be0a7a03519863
aditya140 <theaditya140@gmail.com>
2021-01-23 00:48:43 +0000

    data analysis

--- data_analysis_results.py
+++ data_analysis_results.py
@@ -1,2 +1,146 @@
-def create_error_report(file):
+# %%
+import pandas as pd
 
+
+def create_error_report(file_name):
+
+    dlnd_data = pd.read_csv("dlnd_data.csv")
+    analysis_file = pd.read_csv(file_name)
+
+    analysis_file["pred_cls"] = (1 - analysis_file["pred"]).apply(lambda x: round(x))
+    error_ids = analysis_file[analysis_file["pred_cls"] != analysis_file["true"]]
+
+    error_report = dlnd_data[dlnd_data["id"].isin(error_ids.id.values)]
+    error_report.to_csv(file_name.split(".")[0] + "_report." + file_name.split(".")[1])
+
+
+# %%
+create_error_report("han_analysis.csv")
+# %%
+create_error_report("han_cnn_analysis.csv")
+# %%
+create_error_report("dan_analysis.csv")
+
+# %%
+create_error_report("cnn_analysis.csv")
+# %%
+import pandas as pd
+
+han_cnn_rep = pd.read_csv("han_cnn_analysis_report.csv")
+han_rep = pd.read_csv("han_analysis_report.csv")
+dan_rep = pd.read_csv("dan_analysis_report.csv")
+cnn_rep = pd.read_csv("cnn_analysis_report.csv")
+
+# %%
+
+han_analysis = pd.read_csv("han_analysis.csv")
+han_cnn_analysis = pd.read_csv("han_cnn_analysis.csv")
+dan_analysis = pd.read_csv("dan_analysis.csv")
+cnn_analysis = pd.read_csv("cnn_analysis.csv")
+
+# %%
+def intersection_ids(a,b):
+    return set(a.id).intersection(b.id)
+
+# %%
+print("HAN_CNN errors - ",len(han_cnn_rep.id) )
+print("DAN errors - ",len(dan_rep.id) )
+print("HAN errors - ",len(han_rep.id) )
+print("CNN errors - ",len(cnn_rep.id) )
+# %%
+
+print("HAN - CNN common errors - ",len(intersection_ids(han_rep,cnn_rep)) )
+print("HAN - DAN common errors - ",len(intersection_ids(han_rep,dan_rep)) )
+print("HAN - HAN_CNN common errors - ",len(intersection_ids(han_rep,han_cnn_rep)) )
+print("DAN - CNN common errors - ",len(intersection_ids(dan_rep,cnn_rep)) )
+print("DAN - HAN_CNN common errors - ",len(intersection_ids(dan_rep,han_cnn_rep)) )
+print("HAN_CNN - CNN common errors - ",len(intersection_ids(han_cnn_rep,cnn_rep)) )
+# %%
+
+import nltk
+
+def report_analysis(rep):
+    target_num_words = rep['target_text'].apply(lambda x: len(nltk.word_tokenize(x)))
+    target_num_sent = rep['target_text'].apply(lambda x: len(nltk.sent_tokenize(x)))
+    source_num_words = rep['source'].apply(lambda x: len(nltk.word_tokenize(x)))
+    source_num_sent = rep['source'].apply(lambda x: len(nltk.sent_tokenize(x)))
+    analysis = pd.DataFrame({"target_num_words":target_num_words,"target_num_sent":target_num_sent,"source_num_words":source_num_words,"source_num_sent":source_num_sent})
+    return analysis.describe(percentiles=[])
+# %%
+report_analysis(han_rep)
+
+# %%
+report_analysis(han_cnn_rep)
+# %%
+report_analysis(dan_rep)
+# %%
+
+report_analysis(cnn_rep)
+# %%
+# HAN - HAN_CNN common errors
+report_analysis(han_rep.merge(han_cnn_rep))
+# %%
+# HAN - DAN common errors
+report_analysis(han_rep.merge(dan_rep))
+# %%
+# HAN - CNN common errors
+report_analysis(han_rep.merge(cnn_rep))
+# %%
+
+# DAN - HAN_CNN common errors
+report_analysis(dan_rep.merge(han_cnn_rep))
+
+# %%
+
+# DAN - CNN common errors
+report_analysis(dan_rep.merge(cnn_rep))
+
+# %%
+
+# CNN - HAN_CNN common errors
+report_analysis(cnn_rep.merge(han_cnn_rep))
+
+# %%
+
+# %%
+
+
+
+len(nltk.sent_tokenize('''New Delhi: A day after Delhi Chief Arvind Kejriwal "saluted" Prime Minister Narendra Modi for the 'surgical strikes' by the Army on terror launch pads across the LoC, the Opposition on Tuesday demanded the NDA government to give adequate proof of its much-hyped military action against Pakistan.
+
+UPA did PoK strikes too, it is up to the present govt to give proof of the surgical strikes, former home minister P Chidambaram told News18.
+
+The much-hyped surgical strike on last Wednesday night was not the only time that Army had crossed the LoC to take punitive action, the former home minister said.
+
+A similar 'major strike' took place in January 2013, when the UPA was in power, he added. Chidambaram further said that the then government chose not to go public "in keeping with its policy of strategic restraint ".
+
+The Congress leader, however, cautioned the NDA government against taking political ownership of the surgical strikes and drawing any premature conclusions from the same.
+
+When quizzed about the current deadlock between India and Pakistan, Chidambaram said, ''How long can we stay away from dialogue and non-political ties.''
+
+''No cricket, no films is not a strategy to adopt,'' he added.
+
+While political parties, cutting across their ideological divide, have expressed support to the Narendra Modi government for giving a go ahead to the armed forces for conducting surgical strikes across the LoC against terror launch pads backed by Pakistan, the opposition has also demanded the government to give proof of the military action.
+
+Chidambaram's remarks came a day after Kejriwal, a noted critic of PM Modi, urged the Centre to counter Pakistan's smear campaign on international stage.
+
+Kejriwal, who is often at loggerheads with the Centre, said he may have differences with the Prime Minister over several issues, but by undertaking the surgical strikes, Modi has shown the will to deal with Pakistan.
+
+"Last week our army showed valour and avenged the deaths of 19 soldiers killed in the Uri attack. I may have differences with the Prime Minister over a 100 issues. But when he has shown the will (to deal with this matter), I salute him," he said.
+
+This is perhaps the first time that Kejriwal, who has been critical about the Modi government and its Pakistan policy, has come out praising him. On the day of the strikes, the Delhi Chief Minister had hailed the army, but there was no word of praise for Modi.
+
+A day later, he had told the Delhi Assembly that is time to stand with the Centre and the differences between them can be sorted out later.
+
+Claiming that Pakistan has gone "berserk" after the strike, Kejriwal said, it is resorting to smear campaign against India at international fora and this has to be countered.
+
+"It has resorted to playing dirty politics. Since the last two days, Pakistan is taking international journalists to the border and trying to show that surgical strikes never took place.
+
+"Two days back, the United Nations gave a statement that there was no such activity on the border," he said.
+
+"My blood boiled over these news reports (of the international media). Pakistan is indulging in smearing India's image at the international level.
+
+The UN Military Observer Group in India and Pakistan (UNMOGIP) "has not directly observed" any firing along the LoC, UN chief Ban Ki-moon's spokesperson  Stephane Dujarric had said on September 30 against the backdrop of the surgical strikes conducted by India.
+
+"I appeal to the Prime Minister that the way he and the Army taught Pakistan a lesson on ground, he should also unmask the propaganda by Pakistan at international level. The whole country is with you. I also appeal to the countrymen not to believe in the false campaign by Pakistan," he said.'''))
+# %%
--- novelty/dan/data_analysis.py
+++ novelty/dan/data_analysis.py
@@ -5,7 +5,7 @@ import joblib
 import pickle
 import argparse
 from lang import *
-from novelty.han_cnn.han_cnn import *
+from novelty.dan.dan import *
 from snli.bilstm.bilstm import *
 from snli.attn_enc.attn_enc import *
 from joblib import Memory
@@ -15,14 +15,8 @@ from pytorch_lightning.callbacks import LearningRateLogger
 from pytorch_lightning.profiler import AdvancedProfiler
 from pytorch_lightning.loggers import NeptuneLogger, TensorBoardLogger
 from pytorch_lightning.metrics import Accuracy
-from utils.load_models import (
-    load_bilstm_encoder,
-    load_attn_encoder,
-    load_han_attn_encoder,
-    load_han_bilstm_encoder,
-    reset_model,
-)
-from utils.helpers import seed_torch
+from utils.load_models import load_bilstm_encoder, load_attn_encoder
+from utils.save_models import save_model, save_model_neptune
 from novelty.train_utils import *
 from datamodule import *
 import os
@@ -30,57 +24,60 @@ from utils.keys import NEPTUNE_API
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Novelty HAN Training")
+    parser = argparse.ArgumentParser(description="Novelty CNN Training")
 
     parser.add_argument("--webis", action="store_true", help="Webis dataset")
     parser.add_argument("--dlnd", action="store_true", help="DLND dataset")
-    parser.add_argument("--apwsj", action="store_true", help="APWSJ dataset")
+    parser.add_argument("--apwsj", action="store_true", help="apwsj dataset")
+    parser.add_argument("--save", action="store_true", help="Save model")
     parser.add_argument("--encoder", type=str, help="Encoder Type")
+    parser.add_argument("--log", action="store_true", help="Log to neptune")
     parser.add_argument(
-        "--reset", action="store_true", help="Reset Weights", default=False
+        "--use_nltk", action="store_true", help="Dataset imdb", default=False
     )
-    parser.add_argument("--use_nltk", action="store_true", help="Dataset imdb", default=False)
-    parser.add_argument("--log", action="store_true", help="Dataset imdb")
     args = parser.parse_args()
 
-    use_nltk=args.use_nltk
-    seed_torch()
+    use_nltk = args.use_nltk
 
     if args.encoder == "bilstm":
-        model_id = "DOC-5"
-        encoder, Lang = load_han_bilstm_encoder(model_id)
+        model_id = "SNLI-13"
+        encoder, Lang = load_bilstm_encoder(model_id)
     elif args.encoder == "attention":
-        model_id = "DOC-2"
-        # model_id = "DOC-13"
-        encoder, Lang = load_han_attn_encoder(model_id)
+        model_id = "SNLI-12"
+        encoder, Lang = load_attn_encoder(model_id)
 
     if args.webis:
-        data_module = webis_data_module(Lang,use_nltk=use_nltk)
+        data_module = webis_data_module(Lang, use_nltk=use_nltk)
     elif args.dlnd:
-        data_module = dlnd_data_module(Lang,use_nltk=use_nltk)
+        data_module = dlnd_data_module(Lang, use_nltk=use_nltk)
     elif args.apwsj:
-        data_module = apwsj_data_module(Lang,use_nltk=use_nltk)
+        data_module = apwsj_data_module(Lang, use_nltk=use_nltk)
 
+    # if args.webis:
+    #     data_module = webis_crossval_datamodule(Lang)
+    # elif args.dlnd:
+    #     data_module = dlnd_crossval_datamodule(Lang)
+    # elif args.apwsj:
+    #     data_module = apwsj_crossval_datamodule(Lang)
+    # data_module.set_fold(0)
 
-    data_module.batch_size = 12
     params = {
+        "encoder_dim": encoder.conf.hidden_size,
+        "hidden_size": 400,
+        "dropout": 0.3,
+        "activation": "tanh",
         "optim": "adamw",
         "weight_decay": 0.1,
         "lr": 0.00010869262115700171,
         "scheduler": "lambda",
-        "activation": "tanh",
         "analysis":True,
-        "analysisFile":'han_cnn_analysis.csv',
+        "analysisFile":'dan_analysis.csv'
     }
 
-    model_conf = HAN_CNN_conf(100,encoder, **params)
-    model = Novelty_CNN_model(HAN_CNN, model_conf, params)
+    model_conf = DAN_conf(100, encoder, **params)
+    model = Novelty_CNN_model(DAN, model_conf, params)
 
-    if args.reset:
-        print("Reinitializing weights")
-        model.model = reset_model(model.model)
-
-    EPOCHS = 2
+    EPOCHS = 3
 
     tensorboard_logger = TensorBoardLogger("lightning_logs")
 
@@ -91,20 +88,15 @@ if __name__ == "__main__":
             experiment_name="Evaluation",  # Optional,
             tags=[
                 ("Webis" if args.webis else ("DLND" if args.dlnd else "APWSJ")),
-                "test",
-                "HAN_CNN",
-                "encoder_" + args.encoder,
-                ("weights_reset" if args.reset else "pretrained"),
+                "DAN",
             ],
         )
+        expt_id = neptune_logger.experiment.id
         neptune_logger.experiment.log_metric("epochs", EPOCHS)
         neptune_logger.experiment.log_text("Use NLTK", str(use_nltk))
-        logger = [neptune_logger,tensorboard_logger]
+        loggers = [neptune_logger, tensorboard_logger]
     else:
-        logger = [tensorboard_logger]
-
-
-
+        loggers = [tensorboard_logger]
 
     lr_logger = LearningRateLogger(logging_interval="step")
     trainer = pl.Trainer(
@@ -114,11 +106,12 @@ if __name__ == "__main__":
         profiler=False,
         auto_lr_find=False,
         callbacks=[lr_logger],
-        logger=logger,
+        logger=loggers,
         row_log_interval=2,
     )
     trainer.fit(model, data_module)
-    trainer.test(model, datamodule = data_module)
+    trainer.test(model, datamodule=data_module)
 
-
-
+    model_data = {"model": model.model, "model_conf": model_conf, "Lang": Lang}
+    save_path = save_model("cnn_novelty", expt_id, model_data)
+    save_model_neptune(save_path, neptune_logger)
