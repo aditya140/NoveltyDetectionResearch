7a50de54f98b33b06906402e0050aaeb0b6a48d1
aditya140 <theaditya140@gmail.com>
2021-02-13 06:01:01 +0000

    novelty dataset

---
+++ src/datasets/novelty.py
@@ -0,0 +1,260 @@
+from torchtext import data
+import os
+import six
+import requests
+import csv
+from tqdm import tqdm
+import io
+import zipfile
+import tarfile
+import gzip
+import shutil
+from functools import partial
+import xml.etree.ElementTree as ET
+import glob
+import json
+
+from src.utils.download_utils import download_from_url
+
+
+class NoveltyDataset(data.TabularDataset):
+    urls = []
+    dirname = ""
+    name = "novelty"
+
+    # @classmethod
+    # def create_jsonl(cls, path):
+    #     raise NotImplementedError("Must override with your own create jsonl")
+
+    @staticmethod
+    def sort_key(ex):
+        return data.interleave_keys(len(ex.premise), len(ex.hypothesis))
+
+    @classmethod
+    def download(cls, root, check=None):
+        """Download and unzip an online archive (.zip, .gz, or .tgz).
+
+        Arguments:
+            root (str): Folder to download data to.
+            check (str or None): Folder whose existence indicates
+                that the dataset has already been downloaded, or
+                None to check the existence of root/{cls.name}.
+
+        Returns:
+            str: Path to extracted dataset.
+        """
+        path = os.path.join(root, cls.name)
+        check = path if check is None else check
+        if not os.path.isdir(check):
+            for url in cls.urls:
+                if isinstance(url, tuple):
+                    url, filename = url
+                else:
+                    filename = os.path.basename(url)
+                zpath = os.path.join(path, filename)
+                if not os.path.isfile(zpath):
+                    if not os.path.exists(os.path.dirname(zpath)):
+                        os.makedirs(os.path.dirname(zpath))
+                    print("downloading {}".format(filename))
+                    download_from_url(url, zpath)
+                zroot, ext = os.path.splitext(zpath)
+                _, ext_inner = os.path.splitext(zroot)
+                if ext == ".zip":
+                    with zipfile.ZipFile(zpath, "r") as zfile:
+                        print("extracting")
+                        zfile.extractall(path)
+                # tarfile cannot handle bare .gz files
+                elif ext == ".tgz" or ext == ".gz" and ext_inner == ".tar":
+                    with tarfile.open(zpath, "r:gz") as tar:
+                        dirs = [member for member in tar.getmembers()]
+                        tar.extractall(path=path, members=dirs)
+                elif ext == ".gz":
+                    with gzip.open(zpath, "rb") as gz:
+                        with open(zroot, "wb") as uncompressed:
+                            shutil.copyfileobj(gz, uncompressed)
+
+        return os.path.join(path, cls.dirname)
+
+    @classmethod
+    def splits(
+        cls,
+        text_field,
+        label_field,
+        parse_field=None,
+        extra_fields={},
+        root=".data",
+        train="train.jsonl",
+        validation="val.jsonl",
+        test="test.jsonl",
+    ):
+        """Create dataset objects for splits of the SNLI dataset.
+
+        This is the most flexible way to use the dataset.
+
+        Arguments:
+            text_field: The field that will be used for premise and hypothesis
+                data.
+            label_field: The field that will be used for label data.
+            parse_field: The field that will be used for shift-reduce parser
+                transitions, or None to not include them.
+            extra_fields: A dict[json_key: Tuple(field_name, Field)]
+            root: The root directory that the dataset's zip archive will be
+                expanded into.
+            train: The filename of the train data. Default: 'train.jsonl'.
+            validation: The filename of the validation data, or None to not
+                load the validation set. Default: 'dev.jsonl'.
+            test: The filename of the test data, or None to not load the test
+                set. Default: 'test.jsonl'.
+        """
+        path = cls.download(root)
+        cls.create_jsonl(path)
+
+        if parse_field is None:
+            fields = {
+                "source": ("source", text_field),
+                "target_text": ("target", text_field),
+                "DLA": ("label", label_field),
+            }
+
+        for key in extra_fields:
+            if key not in fields.keys():
+                fields[key] = extra_fields[key]
+
+        return super(NoveltyDataset, cls).splits(
+            path,
+            root,
+            train,
+            validation,
+            test,
+            format="json",
+            fields=fields,
+            filter_pred=lambda ex: ex.label != "-",
+        )
+
+
+class DLND(NoveltyDataset):
+    urls = [
+        "https://drive.google.com/file/d/1q-P3ReGf-yWnKrhb6XQAuMGo39hXlhYG/view?usp=sharing"
+    ]
+    dirname = "dlnd_1.0"
+    name = "dlnd"
+
+    @classmethod
+    def create_json(cls, path):
+        def get_sources(source):
+            source_meta = [
+                "/".join(i.split("/")[:-1])
+                + "/"
+                + i.split("/")[-1].split(".")[0]
+                + ".xml"
+                for i in source
+            ]
+            sources = list(zip(source, source_meta))
+            data = []
+            for x in sources:
+                with open(x[0], mode="r", errors="ignore") as f:
+                    source_text = f.read()
+                root = ET.parse(x[1]).getroot()
+                title = root.findall("feature")[0].get("title")
+                eventname = root.findall("feature")[1].get("eventname")
+                id = x[0].split("/")[-1].split(".")[0]
+                data.append(
+                    {
+                        "id": id,
+                        "eventname": eventname,
+                        "title": title,
+                        "source_text": source_text,
+                    }
+                )
+            return data
+
+        def get_targets(target):
+            target_meta = [
+                "/".join(i.split("/")[:-1])
+                + "/"
+                + i.split("/")[-1].split(".")[0]
+                + ".xml"
+                for i in target
+            ]
+            targets = list(zip(target, target_meta))
+            data = []
+            for x in targets:
+                with open(x[0], mode="r", errors="ignore") as f:
+                    target_text = f.read()
+                # with open(x[1],mode='r',errors='ignore') as f:
+                #     print(f.read())
+                root = ET.parse(x[1]).getroot()
+                novel = root.findall("feature")[2].get("DLA")
+                src_id = root.findall("feature")[0].get("sourceid").split(",")
+                id = x[0].split("/")[-1].split(".")[0]
+                eventname = root.findall("feature")[1].get("eventname")
+                data.append(
+                    {
+                        "id": id,
+                        "eventname": eventname,
+                        "target_text": target_text,
+                        "src_id": src_id,
+                        "DLA": novel,
+                    }
+                )
+            return data
+
+        categories = glob.glob(os.path.join(path, "TAP-DLND-1.0_LREC2018_modified/*"))
+        sources = []
+        targets = []
+        for cat in categories:
+            if os.path.isdir(cat):
+                topics = glob.glob(cat + "/*")
+                for topic in topics:
+                    source = topic + "/source/*.txt"
+                    target = topic + "/target/*.txt"
+                    event_id = topic + "/EventId.txt"
+                    sources += get_sources(glob.glob(source))
+                    targets += get_targets(glob.glob(target))
+
+        source_set = {}
+        for i in sources:
+            source_set[i["id"]] = i
+        target_set = {}
+        for i in targets:
+            target_set[i["id"]] = i
+
+        dataset = {}
+        i = 0
+        for target in target_set.keys():
+            source_text = []
+            if len(target_set[target]["src_id"]) > 0 and target_set[target][
+                "src_id"
+            ] != [""]:
+                for src_id in target_set[target]["src_id"]:
+                    source_text.append(source_set[src_id]["source_text"])
+                dataset[i] = {
+                    "target_text": target_set[target]["target_text"],
+                    "source": source_text,
+                    "DLA": target_set[target]["DLA"],
+                }
+                i += 1
+
+        with open(os.path.join(path, "dlnd.jsonl"), "w") as f:
+            json.dump(list(dataset.values()), f)
+
+    @classmethod
+    def splits(
+        cls,
+        text_field,
+        label_field,
+        parse_field=None,
+        root=".data",
+        train="dlnd.jsonl",
+        validation=None,
+        test=None,
+    ):
+        return super(DLND, cls).splits(
+            text_field,
+            label_field,
+            parse_field=parse_field,
+            root=root,
+            train=train,
+            validation=validation,
+            test=test,
+        )
---
+++ src/utils/download_utils.py
@@ -0,0 +1,60 @@
+import os, requests
+import tqdm
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def download_from_url(url, path):
+    """Download file, with logic (from tensor2tensor) for Google Drive"""
+
+    def process_response(r):
+        chunk_size = 16 * 1024
+        total_size = int(r.headers.get("Content-length", 0))
+        with open(path, "wb") as file:
+            with tqdm(
+                total=total_size, unit="B", unit_scale=1, desc=path.split("/")[-1]
+            ) as t:
+                for chunk in r.iter_content(chunk_size):
+                    if chunk:
+                        file.write(chunk)
+                        t.update(len(chunk))
+
+    if "drive.google.com" not in url:
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, stream=True)
+        process_response(response)
+        return
+
+    print("downloading from Google Drive; may take a few minutes")
+    id_ = url.split("/")[-2]
+    download_file_from_google_drive(id_, destination=path)
+    return
