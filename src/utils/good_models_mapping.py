2988e489e300734cca72471b0202d2f7be5def0d
aditya140 <theaditya140@gmail.com>
2021-03-16 23:06:14 -0400

    good_nli_models

---
+++ src/utils/good_models_mapping.py
@@ -0,0 +1,13 @@
+
+"""
+attention_models:
+    SNLI:
+        spacy       --> NLI-93
+        word_piece  --> NLI-150
+    MNLI:
+        spacy       --> NLI-130
+        word_piece  --> NLI-144
+    ANLI:
+        spacy       --> NLI-157
+        word_piece  -->  NLI-177
+"""
